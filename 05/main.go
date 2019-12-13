f78d1421218c9670bc12e5b5ebd44a757aca67e1
fonglh <fonglh@gmail.com>
2019-12-13 15:05:39 +0800

    Day 05. Add jump instructions.
    Fix bug in program reading that causes the last instruction to be parsed
    wrongly.

--- 05/main.go
+++ 05/main.go
@@ -9,28 +9,20 @@ import (
 )
 
 func main() {
-	fmt.Println(puzzle1()) // answer is 4511442
-	//	fmt.Println(puzzle2())
+	//fmt.Println(puzzle1()) // answer is 4511442
+	fmt.Println(puzzle2())
 }
 
 func puzzle1() int {
-	programInput := readFile()
+	programInput := readFile("05.txt")
 	userInput := 1
 	return computer(programInput, userInput)
 }
 
 func puzzle2() int {
-	for noun := 0; noun < 100; noun++ {
-		for verb := 0; verb < 100; verb++ {
-			input := readFile()
-			result := computer(input, 5)
-			if result == 19690720 {
-				return noun*100 + verb
-			}
-		}
-	}
-	// made up error code
-	return -1
+	programInput := readFile("jump-pos.txt")
+	userInput := 1234
+	return computer(programInput, userInput)
 }
 
 func computer(programInput []int, userInput int) int {
@@ -92,18 +84,62 @@ func computer(programInput []int, userInput int) int {
 				output = programInput[i+1]
 			}
 			fmt.Println("output", output, "IP", i)
+		// jump if true
+		case 5:
+			paramMode1 := (programInput[i] / 100) % 10
+			paramMode2 := (programInput[i] / 1000) % 10
+			var param1 int
+			if paramMode1 == 0 {
+				param1 = programInput[programInput[i+1]]
+			} else {
+				param1 = programInput[i+1]
+			}
+			//set instruction pointer to 2nd param, minus 2 from the for loop
+			if param1 != 0 {
+				if paramMode2 == 0 {
+					i = programInput[programInput[i+2]] - 2
+				} else {
+					i = programInput[i+2] - 2
+				}
+			} else {
+				i++
+			}
+		// jump if false
+		case 6:
+			paramMode1 := (programInput[i] / 100) % 10
+			paramMode2 := (programInput[i] / 1000) % 10
+			var param1 int
+			if paramMode1 == 0 {
+				param1 = programInput[programInput[i+1]]
+			} else {
+				param1 = programInput[i+1]
+			}
+			//set instruction pointer to 2nd param, minus 2 from the for loop
+			if param1 == 0 {
+				if paramMode2 == 0 {
+					i = programInput[programInput[i+2]] - 2
+
+				} else {
+					i = programInput[i+2] - 2
+				}
+			} else {
+				i++
+			}
+		default:
+			fmt.Println("no instruction", opcode)
 		}
 	}
 	return output
 }
 
 // Read puzzle input of comma separated ints and parse into a slice of ints.
-func readFile() []int {
-	buffer, err := ioutil.ReadFile("05.txt")
+func readFile(filename string) []int {
+	buffer, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	inputStringSlice := strings.Split(string(buffer), ",")
+	// Trim ending newline or the last instruction conversion will fail.
+	inputStringSlice := strings.Split(strings.TrimSuffix(string(buffer), "\n"), ",")
 	var input []int
 	for _, i := range inputStringSlice {
 		j, _ := strconv.Atoi(i)
