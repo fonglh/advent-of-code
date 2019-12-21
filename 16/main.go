a728ff563dd8f3185cafe87ef4027a9bc8a9a9d0
fonglh <fonglh@gmail.com>
2019-12-21 11:01:00 +0800

    Day 16. Part 2 with much larger input.
    Required a totally different solution based on patterns observed in the
    input.
    Sped up wave pattern generation from part 1, but it wasn't enough.
    Add 1 of the smaller test cases.

--- 16/main.go
+++ 16/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -11,10 +12,45 @@ import (
 
 func main() {
 	puzzle1()
+	puzzle2()
 }
 
 func puzzle1() {
-	currentPhase := readFile("16.txt")
+	inputList := readFile("16.txt")
+	fmt.Println(fft(inputList)[:8])
+}
+
+func puzzle2() {
+	inputList := readFile("16.txt")
+	offset := messageOffset(inputList)
+	var inputSignal []int
+	for i := 0; i < 10000; i++ {
+		inputSignal = append(inputSignal, inputList...)
+	}
+	inputSignal = inputSignal[offset:]
+	fmt.Println(fft2(inputSignal)[:8])
+}
+
+// Everything before the offset doesn't matter as they're all zeroed out
+// Since the offset is more than halfway through, the matrix is an upside
+// down triangle of 1s.
+// Thus the matrix multiplication just involves adding each element to the nth + 1
+// element from the back.
+func fft2(inputList []int) []int {
+	currentPhase := inputList
+	for phaseNum := 0; phaseNum < 100; phaseNum++ {
+		nextPhase := make([]int, len(currentPhase))
+		nextPhase[len(currentPhase)-1] = currentPhase[len(currentPhase)-1]
+		for i := len(currentPhase) - 2; i >= 0; i-- {
+			nextPhase[i] = abs(nextPhase[i+1]+currentPhase[i]) % 10
+		}
+		currentPhase = nextPhase
+	}
+	return currentPhase
+}
+
+func fft(inputList []int) []int {
+	currentPhase := inputList
 	for phaseNum := 0; phaseNum < 100; phaseNum++ {
 		nextPhase := make([]int, len(currentPhase))
 		for i := range currentPhase {
@@ -22,15 +58,16 @@ func puzzle1() {
 		}
 		currentPhase = nextPhase
 	}
-	fmt.Println(currentPhase[:8])
+	return currentPhase
 }
 
 // generate the nth element. start list numbering from 1
 func nthElement(inputList []int, n int) int {
 	var fftSum int
-	pattern := generatePattern(n, len(inputList))
+	//pattern := generatePattern(n, len(inputList))
 	for i := range inputList {
-		fftSum += inputList[i] * pattern[i]
+		fftSum += inputList[i] * patternDigit(n, i)
+		//fftSum += inputList[i] * pattern[i]
 	}
 	return abs(fftSum % 10)
 }
@@ -48,6 +85,20 @@ func generatePattern(numRepeat, length int) []int {
 	}
 	// offset by 1, as given in instructions
 	return pattern[1:]
+}
+
+func patternDigit(numRepeat, idx int) int {
+	basePattern := []int{0, 1, 0, -1}
+	return basePattern[((idx+1)/numRepeat)%4]
+}
+
+func messageOffset(inputList []int) int {
+	digits := inputList[:7]
+	var offset int
+	for i, digit := range digits {
+		offset += digit * int(math.Pow(10, float64(6-i)))
+	}
+	return offset
 }
 
 // https://www.golangprograms.com/go-program-to-read-a-text-file-character-by-character.html
