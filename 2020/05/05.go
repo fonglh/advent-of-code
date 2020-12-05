647034a311d976c7b18e94eb3602b27e9e0a8e49
fonglh <fonglh@gmail.com>
2020-12-05 15:45:05 +0800

    2020 Day 5. Binary Boarding.

---
+++ 2020/05/05.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	input := readFile()
+
+	fmt.Println(puzzle1(input))
+	fmt.Println(puzzle2(input))
+}
+
+func readFile() []string {
+	file, err := os.Open("05.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var input []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return input
+}
+
+func getSeatId(boardingPass string) int {
+	var seatRow int
+	minRow := 0
+	maxRow := 127
+	for i := 0; i < 7; i++ {
+		seatRow = (minRow + maxRow) / 2
+		if boardingPass[i] == 'F' {
+			maxRow = seatRow
+		} else {
+			minRow = seatRow + 1
+		}
+	}
+	seatRow = (minRow + maxRow) / 2
+
+	var seatCol int
+	minCol := 0
+	maxCol := 7
+	for i := 7; i < 10; i++ {
+		seatCol = (minCol + maxCol) / 2
+		if boardingPass[i] == 'L' {
+			maxCol = seatCol
+		} else {
+			minCol = seatCol + 1
+		}
+	}
+	seatCol = (minCol + maxCol) / 2
+	return seatRow*8 + seatCol
+}
+
+func getSeatRange(input []string) (int, int) {
+	var maxSeatId int
+	minSeatId := 127 * 8
+	for _, boardingPass := range input {
+		seatId := getSeatId(boardingPass)
+		if seatId > maxSeatId {
+			maxSeatId = seatId
+		}
+		if seatId < minSeatId {
+			minSeatId = seatId
+		}
+	}
+	return minSeatId, maxSeatId
+}
+
+func puzzle1(input []string) int {
+	_, maxSeatId := getSeatRange(input)
+	return maxSeatId
+}
+
+func puzzle2(input []string) int {
+	takenSeats := make(map[int]bool)
+	for _, boardingPass := range input {
+		takenSeats[getSeatId(boardingPass)] = true
+	}
+
+	minSeatId, maxSeatId := getSeatRange(input)
+
+	for i := minSeatId; i <= maxSeatId; i++ {
+		if takenSeats[i] == false {
+			return i
+		}
+	}
+	return 0
+}
