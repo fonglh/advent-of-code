5c61b35397b32fb0f1646c5d942f2ffb0d67768d
fonglh <fonglh@gmail.com>
2020-12-12 01:42:49 +0800

    2020 Day 11. Seating System Part 2.
    Just had to modify the threshold and change the `countOccupied` function
    from part 1.
    
    Multiply the direction offsets by a `step` multiplier until it goes out
    of bounds or a seat is reached.

--- 2020/11/11.go
+++ 2020/11/11.go
@@ -12,7 +12,7 @@ func main() {
 	input := readFile("11.txt")
 
 	fmt.Println(puzzle1(input))
-	//	fmt.Println(puzzle2(input))
+	fmt.Println(puzzle2(input))
 }
 
 func puzzle1(input []string) int {
@@ -42,8 +42,6 @@ func puzzle1(input []string) int {
 			return countTotalOccupied(nextLayout)
 		}
 	}
-
-	return 0
 }
 
 func countOccupied(seatLayout []string, rowIndex, colIndex int) int {
@@ -74,8 +72,63 @@ func countTotalOccupied(seatLayout []string) int {
 	return numOccupied
 }
 
-func puzzle2(input []string) int64 {
-	return 0
+func puzzle2(input []string) int {
+	// Deep copy slice or changing 1 slice will change the other as well
+	currLayout := append(make([]string, 0, len(input)), input...)
+	nextLayout := append(make([]string, 0, len(currLayout)), currLayout...)
+
+	for {
+		currLayout = append(make([]string, 0, len(nextLayout)), nextLayout...)
+		nextLayout = append(make([]string, 0, len(currLayout)), currLayout...)
+
+		for rowIndex, row := range currLayout {
+			for colIndex, col := range row {
+				adjacentOccupied := countOccupied2(currLayout, rowIndex, colIndex)
+
+				if col == 'L' && adjacentOccupied == 0 {
+					nextLayout[rowIndex] = nextLayout[rowIndex][:colIndex] + "#" + nextLayout[rowIndex][colIndex+1:]
+				} else if col == '#' && adjacentOccupied >= 5 {
+					nextLayout[rowIndex] = nextLayout[rowIndex][:colIndex] + "L" + nextLayout[rowIndex][colIndex+1:]
+				} else {
+					nextLayout[rowIndex] = nextLayout[rowIndex][:colIndex] + string(col) + nextLayout[rowIndex][colIndex+1:]
+				}
+			}
+		}
+
+		if countTotalOccupied(currLayout) == countTotalOccupied(nextLayout) {
+			return countTotalOccupied(nextLayout)
+		}
+	}
+}
+
+func countOccupied2(seatLayout []string, rowIndex, colIndex int) int {
+	var numOccupied int
+
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			step := 1
+			for {
+				// multiply all the offsets for each direction by `step` and check if out of bounds
+				if (rowIndex+i*step) >= 0 && (rowIndex+i*step) < len(seatLayout) &&
+					(colIndex+j*step) >= 0 && (colIndex+j*step) < len(seatLayout[rowIndex]) &&
+					!(i*step == 0 && j*step == 0) {
+
+					// break out of the stepper loop when a seat (occupied or empty) is encountered
+					if seatLayout[rowIndex+i*step][colIndex+j*step] == '#' {
+						numOccupied += 1
+						break
+					} else if seatLayout[rowIndex+i*step][colIndex+j*step] == 'L' {
+						break
+					}
+				} else { // stop stepping when out of bounds
+					break
+				}
+				step += 1
+			}
+		}
+	}
+
+	return numOccupied
 }
 
 func printLayout(seatLayout []string) {
