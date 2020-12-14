0e403875c3d38bbece2c72061e4b00b60c35d897
fonglh <fonglh@gmail.com>
2020-12-14 22:49:14 +0800

    2020 Day 14 Docking Data Part 2.
    Generating all combinations for X positions after applying bitmask.

--- 2020/14/14.go
+++ 2020/14/14.go
@@ -14,6 +14,7 @@ func main() {
 	input := readFile("14.txt")
 
 	fmt.Println(puzzle1(input))
+	fmt.Println(puzzle2(input))
 }
 
 func puzzle1(input []string) int64 {
@@ -29,11 +30,71 @@ func puzzle1(input []string) int64 {
 		}
 	}
 
+	return sumMemoryValues(memory)
+}
+
+func puzzle2(input []string) int64 {
+	var mask string
+	memory := make(map[int64]int64)
+
+	for _, line := range input {
+		if strings.HasPrefix(line, "mask") {
+			mask = getMaskString(line)
+		} else {
+			address, value := getMemoryInstruction(line)
+			floatingAddress := getFloatingAddress(address, mask)
+			allAddresses := getAllAddresses(floatingAddress)
+			for _, addr := range allAddresses {
+				addrInt, _ := strconv.ParseInt(addr, 2, 64)
+				memory[addrInt] = value
+			}
+		}
+	}
+	return sumMemoryValues(memory)
+}
+
+func sumMemoryValues(memory map[int64]int64) int64 {
 	var sumMemory int64
 	for _, value := range memory {
 		sumMemory += value
 	}
 	return sumMemory
+}
+
+func getFloatingAddress(rawAddress int64, mask string) string {
+	addressStr := fmt.Sprintf("%036b", rawAddress)
+	floatingAddress := make([]byte, 36)
+
+	for i := 0; i < 36; i += 1 {
+		if mask[i] == '1' {
+			floatingAddress[i] = '1'
+		} else if mask[i] == 'X' {
+			floatingAddress[i] = 'X'
+		} else {
+			floatingAddress[i] = addressStr[i]
+		}
+	}
+
+	return string(floatingAddress)
+}
+
+func getAllAddresses(floatingAddress string) []string {
+	allAddresses := make([]string, 0)
+	numX := strings.Count(floatingAddress, "X")
+	maxCounter := 1 << numX
+	formatString := fmt.Sprintf("%%0%dv", numX) //to zero pad all the possible X values
+
+	for i := 0; i < maxCounter; i++ {
+		currAddress := floatingAddress
+		iStr := fmt.Sprintf(formatString, strconv.FormatInt(int64(i), 2))
+		for j := 0; j < len(iStr); j++ {
+			// Replace 1 X at a time with the bit values of the counter
+			currAddress = strings.Replace(currAddress, "X", string(iStr[j]), 1)
+		}
+		allAddresses = append(allAddresses, currAddress)
+	}
+
+	return allAddresses
 }
 
 func getMaskString(line string) string {
