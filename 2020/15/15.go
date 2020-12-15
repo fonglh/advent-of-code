cb9197d45054b3b862b0147353a9aa98c722277c
fonglh <fonglh@gmail.com>
2020-12-15 22:50:24 +0800

    2020 Day 15. Rambunctious Recitation Part 2.
    Need to change from O(n^2) algo (from the repeated array search) to O(n)
    algo (using a map).

--- 2020/15/15.go
+++ 2020/15/15.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	input := []int{14, 8, 16, 0, 1, 17}
-	//input := []int{0, 3, 6}
+	//input := []int{1, 3, 2}
 
-	fmt.Println(puzzle1(input))
+	//fmt.Println(puzzle1(input))
+	fmt.Println(puzzle2(input))
 }
 
 func puzzle1(input []int) int {
@@ -35,4 +36,33 @@ func search(arr []int, item int) int {
 		}
 	}
 	return maxIndex
+}
+
+func puzzle2(input []int) int {
+	// Map number to the turn number. Turn numbers start from 1
+	spoken := make(map[int]int)
+	lastNumber := input[len(input)-1]
+
+	// init map
+	for index := 0; index < len(input)-1; index += 1 {
+		spoken[input[index]] = index + 1 // remember turn numbers start from 1
+	}
+
+	for turn := len(input) + 1; turn <= 30000000; turn += 1 {
+		// do the lookup first, then add the previous turn's number to the map
+		index, ok := spoken[lastNumber]
+		spoken[lastNumber] = turn - 1
+
+		if ok {
+			// found, update current turn's number to the difference between prev turn number and index
+			// index is the turn where it was most recently spoken before the previous turn
+			lastNumber = turn - 1 - index
+		} else {
+			// not found, current turn's number is 0
+			lastNumber = 0
+		}
+		// don't add to the map yet. the lookup needs to be done first.
+	}
+
+	return lastNumber
 }
