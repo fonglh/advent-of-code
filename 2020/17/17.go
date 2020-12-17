ca5a91d9d7f503d864b0d0743e2dcec71c1ecb8b
fonglh <fonglh@gmail.com>
2020-12-17 23:53:01 +0800

    2020 Day 17. Conway Cubes Part 2.
    Adapt for 4 dimensions to solve part 2.
    Part 1's 3D code can still work by setting the w coord to 0.

--- 2020/17/17.go
+++ 2020/17/17.go
@@ -12,13 +12,14 @@ type Coordinate struct {
 	x int
 	y int
 	z int
+	w int
 }
 
 func main() {
 	input := readFile("17.txt")
 
 	fmt.Println(puzzle1(input))
-	//fmt.Println(puzzle2(input))
+	fmt.Println(puzzle2(input))
 }
 
 func puzzle1(input []string) int {
@@ -31,7 +32,7 @@ func puzzle1(input []string) int {
 		for x := minCoord.x; x <= maxCoord.x; x += 1 {
 			for y := minCoord.y; y <= maxCoord.y; y += 1 {
 				for z := minCoord.z; z <= maxCoord.z; z += 1 {
-					currCoord := Coordinate{x, y, z}
+					currCoord := Coordinate{x, y, z, 0}
 					numNeighbours := countActiveNeighbours(dimension, currCoord)
 					if dimension[currCoord] && (numNeighbours == 2 || numNeighbours == 3) {
 						dimensionCopy[currCoord] = true
@@ -62,7 +63,45 @@ func puzzle1(input []string) int {
 }
 
 func puzzle2(input []string) int {
-	return 0
+	dimension := initDimension(input)
+
+	for i := 0; i < 6; i++ {
+		dimensionCopy := make(map[Coordinate]bool)
+
+		minCoord, maxCoord := searchDimension(dimension)
+		for x := minCoord.x; x <= maxCoord.x; x += 1 {
+			for y := minCoord.y; y <= maxCoord.y; y += 1 {
+				for z := minCoord.z; z <= maxCoord.z; z += 1 {
+					for w := minCoord.w; w <= maxCoord.w; w += 1 {
+						currCoord := Coordinate{x, y, z, w}
+						numNeighbours := countActiveNeighbours(dimension, currCoord)
+						if dimension[currCoord] && (numNeighbours == 2 || numNeighbours == 3) {
+							dimensionCopy[currCoord] = true
+						} else if !dimension[currCoord] && numNeighbours == 3 {
+							dimensionCopy[currCoord] = true
+						} else {
+							dimensionCopy[currCoord] = false
+						}
+					}
+				}
+			}
+		}
+
+		// reset dimension and copy new state over
+		dimension = make(map[Coordinate]bool)
+		for k, v := range dimensionCopy {
+			dimension[k] = v
+		}
+	}
+
+	activeCount := 0
+	for _, v := range dimension {
+		if v {
+			activeCount += 1
+		}
+	}
+
+	return activeCount
 }
 
 func initDimension(input []string) map[Coordinate]bool {
@@ -71,7 +110,7 @@ func initDimension(input []string) map[Coordinate]bool {
 	for y, row := range input {
 		for x, cell := range row {
 			if cell == '#' {
-				dimension[Coordinate{x, y, 0}] = true
+				dimension[Coordinate{x, y, 0, 0}] = true
 			}
 		}
 	}
@@ -85,10 +124,12 @@ func countActiveNeighbours(dimension map[Coordinate]bool, coord Coordinate) int 
 	for dx := -1; dx <= 1; dx += 1 {
 		for dy := -1; dy <= 1; dy += 1 {
 			for dz := -1; dz <= 1; dz += 1 {
-				if !(dx == 0 && dy == 0 && dz == 0) {
-					checkCoord := Coordinate{coord.x + dx, coord.y + dy, coord.z + dz}
-					if dimension[checkCoord] {
-						count += 1
+				for dw := -1; dw <= 1; dw += 1 {
+					if !(dx == 0 && dy == 0 && dz == 0 && dw == 0) {
+						checkCoord := Coordinate{coord.x + dx, coord.y + dy, coord.z + dz, coord.w + dw}
+						if dimension[checkCoord] {
+							count += 1
+						}
 					}
 				}
 			}
@@ -100,8 +141,8 @@ func countActiveNeighbours(dimension map[Coordinate]bool, coord Coordinate) int 
 
 // return min and max x,y,z coordinates to search
 func searchDimension(dimension map[Coordinate]bool) (Coordinate, Coordinate) {
-	minCoord := Coordinate{math.MaxInt32, math.MaxInt32, math.MaxInt32}
-	maxCoord := Coordinate{math.MinInt32, math.MinInt32, math.MinInt32}
+	minCoord := Coordinate{math.MaxInt32, math.MaxInt32, math.MaxInt32, math.MaxInt32}
+	maxCoord := Coordinate{math.MinInt32, math.MinInt32, math.MinInt32, math.MinInt32}
 
 	for k, _ := range dimension {
 		if k.x < minCoord.x {
@@ -113,6 +154,9 @@ func searchDimension(dimension map[Coordinate]bool) (Coordinate, Coordinate) {
 		if k.z < minCoord.z {
 			minCoord.z = k.z
 		}
+		if k.w < minCoord.w {
+			minCoord.w = k.w
+		}
 
 		if k.x > maxCoord.x {
 			maxCoord.x = k.x
@@ -123,10 +167,13 @@ func searchDimension(dimension map[Coordinate]bool) (Coordinate, Coordinate) {
 		if k.z > maxCoord.z {
 			maxCoord.z = k.z
 		}
+		if k.w > maxCoord.w {
+			maxCoord.w = k.w
+		}
 	}
 
-	minCoord = Coordinate{minCoord.x - 1, minCoord.y - 1, minCoord.z - 1}
-	maxCoord = Coordinate{maxCoord.x + 1, maxCoord.y + 1, maxCoord.z + 1}
+	minCoord = Coordinate{minCoord.x - 1, minCoord.y - 1, minCoord.z - 1, minCoord.w - 1}
+	maxCoord = Coordinate{maxCoord.x + 1, maxCoord.y + 1, maxCoord.z + 1, maxCoord.w + 1}
 
 	return minCoord, maxCoord
 }
