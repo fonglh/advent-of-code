dfe442045f6b9d3ba6560f3bbb297966cd8df846
fonglh <fonglh@gmail.com>
2020-12-31 01:24:42 +0800

    2020 Day 23. Crab Cups Part 2.
    Use optimized version of puzzle1 to solve puzzle 2.

--- 2020/23/23.go
+++ 2020/23/23.go
@@ -10,7 +10,7 @@ func main() {
 	//input = [9]int{3, 8, 9, 1, 2, 5, 4, 6, 7}
 
 	fmt.Println(puzzle1(input))
-	//fmt.Println(puzzle2(input))
+	fmt.Println(puzzle2(input))
 }
 
 func puzzle1(input [9]int) int {
@@ -71,30 +71,29 @@ func printRing(r *ring.Ring) {
 }
 
 func puzzle2(input [9]int) int {
-	// init crab ring
+	// init crab ring and nodemap
 	crabRing := ring.New(1000000)
+	nodeMap := make(map[int]*ring.Ring)
 	for _, num := range input {
 		crabRing.Value = num
+		nodeMap[num] = crabRing
 		crabRing = crabRing.Next()
 	}
 
 	for num := 10; num <= 1000000; num += 1 {
 		crabRing.Value = num
+		nodeMap[num] = crabRing
 		crabRing = crabRing.Next()
 	}
 
-	firstCup := crabRing
-
-	for count := 0; count < 10; count += 1 {
-		fmt.Println("move", count+1)
-		//		fmt.Println("current", crabRing.Value)
+	for count := 0; count < 10000000; count += 1 {
+		//if count%10000 == 0 {
+		//	fmt.Println("move", count+1)
+		//}
 		removed := crabRing.Unlink(3)
-		//		fmt.Println("removed")
-		//		printRing(removed)
 
 		// find destination candidate
 		destCandidate := crabRing.Value.(int) - 1
-		destination := crabRing.Move(1)
 		for {
 			foundDestCandidate := true
 			// hard code the length as Len() runs in O(n) time and the length is fixed anyway
@@ -107,28 +106,24 @@ func puzzle2(input [9]int) int {
 			if !foundDestCandidate || destCandidate == 0 {
 				destCandidate -= 1
 				if destCandidate <= 0 {
-					destCandidate = 9
+					destCandidate = 1000000
 				}
 			} else {
 				break
 			}
 		}
-		//		fmt.Println("destination", destCandidate)
 
 		//find destination
-		for destination.Value.(int) != destCandidate {
-			destination = destination.Move(1)
-		}
-
+		destination := nodeMap[destCandidate]
 		destination.Link(removed)
 		crabRing = crabRing.Move(1)
-
-		fmt.Println("****************************")
 	}
 
 	product := 1
+	firstCup := nodeMap[1]
 	for i := 0; i < 2; i += 1 {
 		firstCup = firstCup.Move(1)
+		fmt.Println(firstCup.Value.(int))
 		product *= firstCup.Value.(int)
 	}
 
