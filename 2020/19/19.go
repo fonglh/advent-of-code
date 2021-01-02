649ada9c8e132cf2c997d84dd6f14df082aa8962
fonglh <fonglh@gmail.com>
2021-01-03 01:23:03 +0800

    2020 Day 19. Monster Message Part 2.
    Edited rules 8 and 11 like the question asks for.
    Wrote generic match function from https://github.com/mebeim/aoc/blob/master/2020/README.md#part-2---real-solution

--- 2020/19/19.go
+++ 2020/19/19.go
@@ -13,8 +13,10 @@ func main() {
 	rules := readFile("rules.txt")
 	messages := readFile("19.txt")
 
-	fmt.Println(puzzle1(rules, messages))
-	fmt.Println(puzzle2(rules))
+	//fmt.Println(puzzle1(rules, messages))
+	// Retry puzzle 1 using the match function
+	fmt.Println(puzzle1Retry(rules, messages))
+	fmt.Println(puzzle2(rules, messages))
 }
 
 func puzzle1(rules, messages []string) int {
@@ -121,8 +123,105 @@ func strSumProduct(list1, list2 []string) []string {
 	return combined
 }
 
-func puzzle2(input []string) int {
-	return 0
+func puzzle1Retry(rules, messages []string) int {
+	// build rules dictionary
+	rulesMap := make(map[int][][]string)
+	for _, rule := range rules {
+		ruleNum, ruleList := parseRule(rule)
+		rulesMap[ruleNum] = ruleList
+	}
+
+	var numMatch int
+	for _, msg := range messages {
+		matchIndices := match(rulesMap, msg, 0, 0)
+		if contains(matchIndices, len(msg)) {
+			numMatch += 1
+		}
+	}
+
+	return numMatch
+}
+
+func puzzle2(rules, messages []string) int {
+	// build rules dictionary
+	rulesMap := make(map[int][][]string)
+	for _, rule := range rules {
+		ruleNum, ruleList := parseRule(rule)
+		// special handling for rules 8 and 11
+		if ruleNum == 8 {
+			ruleList = append(ruleList, []string{"42", "8"})
+		} else if ruleNum == 11 {
+			ruleList = append(ruleList, []string{"42", "11", "31"})
+		}
+		rulesMap[ruleNum] = ruleList
+	}
+
+	var numMatch int
+
+	for _, msg := range messages {
+		matchIndices := match(rulesMap, msg, 0, 0)
+		// match is found if the message length is one of the match indices.
+		// this means at least 1 path matched all the characters of the message and returned the length
+		// of the message after matching the last index
+		if contains(matchIndices, len(msg)) {
+			numMatch += 1
+		}
+	}
+
+	return numMatch
+}
+
+// return a list of indices from which the matching should continue
+// https://github.com/mebeim/aoc/blob/master/2020/README.md#part-2---real-solution
+func match(rulesMap map[int][][]string, message string, ruleNum int, index int) []int {
+	// Past the end of the string, cannot match anything
+	if index >= len(message) {
+		return []int{}
+	}
+
+	// if current rule is a simple character, match that literally
+	subRules := rulesMap[ruleNum]
+	if subRules[0][0] == "\"a\"" && message[index] == 'a' ||
+		subRules[0][0] == "\"b\"" && message[index] == 'b' {
+		// matches a single char. advance by 1 and return.
+		return []int{index + 1}
+	} else if subRules[0][0] == "\"a\"" || subRules[0][0] == "\"b\"" {
+		// rule is a simple character but the current character doesn't match, cannot continue matching
+		return []int{}
+	}
+
+	// if we're here, we need to check the more complicated rule format X: A B | C D
+	matches := make([]int, 0)
+	for _, subRule := range subRules {
+		// start matching from the current position
+		subMatches := []int{index}
+
+		for _, subRuleComponent := range subRule {
+			// get all resulting positions after matching this rule from any of the possible positions we have so far
+			newMatches := make([]int, 0)
+			for _, idx := range subMatches {
+				subRuleComponentNum, _ := strconv.Atoi(subRuleComponent)
+				newMatches = append(newMatches, match(rulesMap, message, subRuleComponentNum, idx)...)
+			}
+
+			// keep the new positions, continue matching the next subrule component
+			subMatches = newMatches
+		}
+
+		// collect all possible matches for this subrule and add them to the final result
+		matches = append(matches, subMatches...)
+	}
+
+	return matches
+}
+
+func contains(intList []int, number int) bool {
+	for _, item := range intList {
+		if item == number {
+			return true
+		}
+	}
+	return false
 }
 
 func readFile(filename string) []string {
