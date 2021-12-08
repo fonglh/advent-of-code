cf46eed5f86db91d1ba8df0a869f675b86d66496
fonglh <fonglh@gmail.com>
2021-12-09 02:25:17 +0800

    2021 Day 08. Seven Segment Search refactor.
    Refactor part 2 into functions to make it more readable.

--- 2021/day08_seven_segment_search/bin/day08_seven_segment_search.dart
+++ 2021/day08_seven_segment_search/bin/day08_seven_segment_search.dart
@@ -18,71 +18,8 @@ void main(List<String> arguments) async {
     outputValues.add(outputValue);
   }
 
-  // https://www.reddit.com/r/adventofcode/comments/rbj87a/2021_day_8_solutions/hnpad75/
-  Map<String, int> countsToDigitsMap = {
-    '467889': 0,
-    '89': 1,
-    '47788': 2,
-    '77889': 3,
-    '6789': 4,
-    '67789': 5,
-    '467789': 6,
-    '889': 7,
-    '4677889': 8,
-    '677889': 9
-  };
-
-  //print(part1(outputValues));
-
-  // Very messy part 2
-  int outputSum = 0;
-
-  for (int i = 0; i < signalPatterns.length; i++) {
-    var signalPattern = signalPatterns[i];
-    var outputValue = outputValues[i];
-    Map<String, int> segmentCount = {
-      "a": 0,
-      "b": 0,
-      "c": 0,
-      "d": 0,
-      "e": 0,
-      "f": 0,
-      "g": 0
-    };
-
-    Map<String, int> outputLookup = {};
-
-    signalPatterns[i].join("").split("").forEach((char) {
-      segmentCount[char] = segmentCount[char]! + 1;
-    });
-    signalPatterns[i].forEach((pattern) {
-      List<int> digitKey = [];
-      pattern.split("").forEach((char) {
-        digitKey.add(segmentCount[char]!);
-      });
-      digitKey.sort();
-      outputLookup[pattern] = countsToDigitsMap[digitKey.join("")]!;
-    });
-    // print(outputLookup);
-
-    String outputDigits = "";
-    outputValues[i].forEach((element) {
-      Set elementSet = Set();
-      elementSet.addAll(element.split(""));
-
-      for (var key in outputLookup.keys) {
-        Set keySet = Set();
-        keySet.addAll(key.split(""));
-        if (setEquals(keySet, elementSet)) {
-          outputDigits += outputLookup[key].toString();
-        }
-      }
-    });
-
-    outputSum += int.parse(outputDigits);
-  }
-
-  print(outputSum);
+  print(part1(outputValues));
+  print(part2(signalPatterns, outputValues));
 }
 
 int part1(List<List<String>> outputValues) {
@@ -98,6 +35,91 @@ int part1(List<List<String>> outputValues) {
     }
   }
   return countDigitsWithUniqueNumberOfSegments;
+}
+
+int part2(List<List<String>> signalPatterns, List<List<String>> outputValues) {
+  int outputSum = 0;
+
+  for (int i = 0; i < signalPatterns.length; i++) {
+    var signalPattern = signalPatterns[i];
+    var outputValue = outputValues[i];
+
+    Map<String, int> segmentCount = getSegmentCounts(signalPattern);
+
+    Map<String, int> outputLookup =
+        buildOutputLookup(segmentCount, signalPattern);
+
+    outputSum += getNumericalOutputValue(outputLookup, outputValue);
+  }
+
+  return outputSum;
+}
+
+Map<String, int> getSegmentCounts(List<String> signalPattern) {
+  Map<String, int> segmentCount = {
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0
+  };
+
+  signalPattern.join("").split("").forEach((char) {
+    segmentCount[char] = segmentCount[char]! + 1;
+  });
+
+  return segmentCount;
+}
+
+// Maps each signal pattern to its display value on a 7-segment display
+Map<String, int> buildOutputLookup(
+    Map<String, int> segmentCount, List<String> signalPattern) {
+  // https://www.reddit.com/r/adventofcode/comments/rbj87a/2021_day_8_solutions/hnpad75/
+  Map<String, int> countsToDigitsMap = {
+    '467889': 0,
+    '89': 1,
+    '47788': 2,
+    '77889': 3,
+    '6789': 4,
+    '67789': 5,
+    '467789': 6,
+    '889': 7,
+    '4677889': 8,
+    '677889': 9
+  };
+  Map<String, int> outputLookup = {};
+
+  signalPattern.forEach((pattern) {
+    List<int> digitKey = [];
+    pattern.split("").forEach((char) {
+      digitKey.add(segmentCount[char]!);
+    });
+    digitKey.sort();
+    outputLookup[pattern] = countsToDigitsMap[digitKey.join("")]!;
+  });
+  // print(outputLookup);
+  return outputLookup;
+}
+
+int getNumericalOutputValue(
+    Map<String, int> outputLookup, List<String> outputValue) {
+  String outputDigits = "";
+  outputValue.forEach((element) {
+    Set elementSet = Set();
+    elementSet.addAll(element.split(""));
+
+    for (var key in outputLookup.keys) {
+      Set keySet = Set();
+      keySet.addAll(key.split(""));
+      if (setEquals(keySet, elementSet)) {
+        outputDigits += outputLookup[key].toString();
+      }
+    }
+  });
+
+  return int.parse(outputDigits);
 }
 
 bool setEquals(Set a, Set b) {
