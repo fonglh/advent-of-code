4f0f39e093681f9a790a1e5fd55b7514195e084a
fonglh <fonglh@gmail.com>
2021-12-09 02:05:08 +0800

    2021 Day 08. Seven Segment Search Part 2.
    Very messy code, implemented solution from https://www.reddit.com/r/adventofcode/comments/rbj87a/2021_day_8_solutions/hnpad75/
    
    Count the number of occurences of each character in the signal patterns.
    Substitute each character in the signal pattern with its count.
    For each signal pattern digit, sort the substituted counts.
    This creates a unique key that corresponds to the displayed digit, given
    in the hardcoded map.
    
    Do a set lookup on the signal pattern strings to get the output digits.

--- 2021/day08_seven_segment_search/bin/day08_seven_segment_search.dart
+++ 2021/day08_seven_segment_search/bin/day08_seven_segment_search.dart
@@ -18,6 +18,74 @@ void main(List<String> arguments) async {
     outputValues.add(outputValue);
   }
 
+  // https://www.reddit.com/r/adventofcode/comments/rbj87a/2021_day_8_solutions/hnpad75/
+  Map<String, int> countsToDigitsMap = {
+    '467889': 0,
+    '89': 1,
+    '47788': 2,
+    '77889': 3,
+    '6789': 4,
+    '67789': 5,
+    '467789': 6,
+    '889': 7,
+    '4677889': 8,
+    '677889': 9
+  };
+
+  //print(part1(outputValues));
+
+  // Very messy part 2
+  int outputSum = 0;
+
+  for (int i = 0; i < signalPatterns.length; i++) {
+    var signalPattern = signalPatterns[i];
+    var outputValue = outputValues[i];
+    Map<String, int> segmentCount = {
+      "a": 0,
+      "b": 0,
+      "c": 0,
+      "d": 0,
+      "e": 0,
+      "f": 0,
+      "g": 0
+    };
+
+    Map<String, int> outputLookup = {};
+
+    signalPatterns[i].join("").split("").forEach((char) {
+      segmentCount[char] = segmentCount[char]! + 1;
+    });
+    signalPatterns[i].forEach((pattern) {
+      List<int> digitKey = [];
+      pattern.split("").forEach((char) {
+        digitKey.add(segmentCount[char]!);
+      });
+      digitKey.sort();
+      outputLookup[pattern] = countsToDigitsMap[digitKey.join("")]!;
+    });
+    // print(outputLookup);
+
+    String outputDigits = "";
+    outputValues[i].forEach((element) {
+      Set elementSet = Set();
+      elementSet.addAll(element.split(""));
+
+      for (var key in outputLookup.keys) {
+        Set keySet = Set();
+        keySet.addAll(key.split(""));
+        if (setEquals(keySet, elementSet)) {
+          outputDigits += outputLookup[key].toString();
+        }
+      }
+    });
+
+    outputSum += int.parse(outputDigits);
+  }
+
+  print(outputSum);
+}
+
+int part1(List<List<String>> outputValues) {
   int countDigitsWithUniqueNumberOfSegments = 0;
   for (var output in outputValues) {
     for (var digit in output) {
@@ -29,7 +97,13 @@ void main(List<String> arguments) async {
       }
     }
   }
-  print(countDigitsWithUniqueNumberOfSegments);
+  return countDigitsWithUniqueNumberOfSegments;
+}
+
+bool setEquals(Set a, Set b) {
+  if (a.length != b.length) return false;
+
+  return a.containsAll(b.toList());
 }
 
 Future<List<String>> readFile(String path) async {
