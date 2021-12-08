ab4a3e848abe08d83489e9380068c31dbb434ba9
fonglh <fonglh@gmail.com>
2021-12-09 00:37:14 +0800

    2021 Day 07. Treachery of Whales Part 2.
    Arithmetic progression formula.
    Need to change initial minFuel calculation to use the new formula or it
    will be too low.

--- 2021/day07_treachery_of_whales/bin/day07_treachery_of_whales.dart
+++ 2021/day07_treachery_of_whales/bin/day07_treachery_of_whales.dart
@@ -9,15 +9,17 @@ void main(List<String> arguments) async {
       input[0].split(",").map((e) => int.parse(e)).toList();
 
   int maxPosition = crabPositions.reduce(max);
-  // Initialise assuming all align on position 1.
-  int minFuel = crabPositions.reduce((a, b) => a + b);
+  // Initialise assuming all align on position 0.
+  //int minFuel = crabPositions.reduce((a, b) => a + b);
+  int minFuel =
+      crabPositions.map((pos) => partTwoFuel(pos, 0)).reduce((a, b) => a + b);
   int minPos = 0;
 
   for (int position = 1; position <= maxPosition; position++) {
     int fuelRequired = crabPositions
-        .map((pos) => ((pos - position)).abs())
-        .toList()
+        .map((pos) => partTwoFuel(pos, position))
         .reduce((a, b) => a + b);
+    //print('$position: $fuelRequired');
     if (fuelRequired < minFuel) {
       minFuel = fuelRequired;
       minPos = position;
@@ -26,6 +28,15 @@ void main(List<String> arguments) async {
 
   print(minFuel);
   print(minPos);
+}
+
+int partOneFuel(int currPos, int target) {
+  return (currPos - target).abs();
+}
+
+int partTwoFuel(int currPos, int target) {
+  int steps = (currPos - target).abs();
+  return (steps * (steps + 1) ~/ 2);
 }
 
 Future<List<String>> readFile(String path) async {
