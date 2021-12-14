e888e6675800c5f08cd357a3f9f26455aca7a787
fonglh <fonglh@gmail.com>
2021-12-14 19:51:14 +0800

    2021 Day 14. Extended Polymerization part 2.
    Solve the very large string problem in part 2 by using maps to count
    pair and character frequencies.

--- 2021/day14_extended_polymerization/bin/day14_extended_polymerization.dart
+++ 2021/day14_extended_polymerization/bin/day14_extended_polymerization.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 void main(List<String> arguments) async {
   final input = await readFile("14.txt");
-  //List<String> polymerTemplate = "NNCB".split("").toList();
+  // List<String> polymerTemplate = "NNCB".split("").toList();
   List<String> polymerTemplate = "VPPHOPVVSFSVFOCOSBKF".split("").toList();
 
   Map<String, String> insertionRules = {};
@@ -12,15 +12,28 @@ void main(List<String> arguments) async {
     insertionRules[ruleParts[0]] = ruleParts[1];
   }
 
+  print(part1(polymerTemplate, insertionRules));
+
+  // --------------------- Part 2 ----------------------------------------
+  // polymerTemplate = "NNCB".split("").toList();
+  polymerTemplate = "VPPHOPVVSFSVFOCOSBKF".split("").toList();
+  var polymerPairFrequencies = pairFrequencyMap(polymerTemplate);
+  var charCounts = frequencyMap(polymerTemplate);
+
+  for (int step = 0; step < 40; step++) {
+    polymerPairFrequencies =
+        processPolymerMap(polymerPairFrequencies, charCounts, insertionRules);
+  }
+  print(findDifference(charCounts));
+}
+
+int part1(List<String> polymerTemplate, Map<String, String> insertionRules) {
   for (int step = 0; step < 10; step++) {
-    print(step);
     processPolymer(polymerTemplate, insertionRules);
   }
 
   var polymerFrequencies = frequencyMap(polymerTemplate);
-  print(polymerFrequencies);
-
-  // Manually inspect frequencies, the answer for part 1 is 2233.
+  return findDifference(polymerFrequencies);
 }
 
 void processPolymer(
@@ -34,6 +47,49 @@ void processPolymer(
   }
 }
 
+Map<String, int> processPolymerMap(Map<String, int> polymerPairFrequencies,
+    Map<String, int> charCounts, Map<String, String> insertionRules) {
+  Map<String, int> newMap = {...polymerPairFrequencies};
+
+  for (String key in polymerPairFrequencies.keys) {
+    if (polymerPairFrequencies[key]! > 0 && insertionRules.containsKey(key)) {
+      // Remove the original pair
+      newMap[key] = newMap[key]! - polymerPairFrequencies[key]!;
+
+      // Add new pairs
+      String pair1 = key[0] + insertionRules[key]!;
+      String pair2 = insertionRules[key]! + key[1];
+
+      //print("${key} -> ${insertionRules[key]}");
+      if (charCounts.containsKey(insertionRules[key])) {
+        charCounts[insertionRules[key]!] =
+            charCounts[insertionRules[key]!]! + polymerPairFrequencies[key]!;
+      } else {
+        charCounts[insertionRules[key]!] = polymerPairFrequencies[key]!;
+      }
+
+      if (newMap.containsKey(pair1)) {
+        newMap[pair1] = newMap[pair1]! + polymerPairFrequencies[key]!;
+      } else {
+        newMap[pair1] = polymerPairFrequencies[key]!;
+      }
+      if (newMap.containsKey(pair2)) {
+        newMap[pair2] = newMap[pair2]! + polymerPairFrequencies[key]!;
+      } else {
+        newMap[pair2] = polymerPairFrequencies[key]!;
+      }
+    }
+  }
+
+  return newMap;
+}
+
+int findDifference(Map<String, int> charCounts) {
+  var counts = charCounts.values.toList();
+  counts.sort();
+  return counts.last - counts.first;
+}
+
 Map<String, int> frequencyMap(List<String> polymerTemplate) {
   Map<String, int> result = {};
 
@@ -42,6 +98,21 @@ Map<String, int> frequencyMap(List<String> polymerTemplate) {
       result[ch] = result[ch]! + 1;
     } else {
       result[ch] = 1;
+    }
+  }
+
+  return result;
+}
+
+Map<String, int> pairFrequencyMap(List<String> polymerTemplate) {
+  Map<String, int> result = {};
+
+  for (int i = 0; i < polymerTemplate.length - 1; i++) {
+    String pair = polymerTemplate[i] + polymerTemplate[i + 1];
+    if (result.containsKey(pair)) {
+      result[pair] = result[pair]! + 1;
+    } else {
+      result[pair] = 1;
     }
   }
 
