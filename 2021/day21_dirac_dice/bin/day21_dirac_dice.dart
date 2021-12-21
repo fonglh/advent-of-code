ef77e1ddee7095ada3aa55c29844ecc51bd50258
fonglh <fonglh@gmail.com>
2021-12-21 14:55:12 +0800

    2021 Day 21. Dirac Dice part 2.
    Recursive solution, using a Point to track the number of times each
    player wins.
    Precompute the roll results and the number of times they appear to
    reduce the number of recursive calls made.
    This is necessary to even get an answer.

--- 2021/day21_dirac_dice/bin/day21_dirac_dice.dart
+++ 2021/day21_dirac_dice/bin/day21_dirac_dice.dart
@@ -1,4 +1,5 @@
 import '../lib/deterministic_dice.dart';
+import 'dart:math';
 
 void main(List<String> arguments) {
   // puzzle input
@@ -6,9 +7,72 @@ void main(List<String> arguments) {
   int p2Pos = 10;
 
   // test input
-  //p1Pos = 4;
-  //p2Pos = 8;
+  // p1Pos = 4;
+  // p2Pos = 8;
 
+//  print(part1(p1Pos, p2Pos));
+  Point result = Point(0, 0);
+
+  // p2 goes 1st with a dummy roll of 0 value, so the actual game has p1 going first.
+  result = diracGame(p1Pos, p2Pos, 0, 0, 0, false);
+  if (result.x > result.y) {
+    print(result.x);
+  } else {
+    print(result.y);
+  }
+}
+
+Point diracGame(
+    int p1Pos, int p2Pos, int p1Score, int p2Score, int roll, bool p1Turn) {
+  //print("player 1: $p1Pos, $p1Score");
+  //print("player 2: $p2Pos, $p2Score");
+  //print("roll: $roll, p1Turn: $p1Turn");
+  // move players, check for win
+  if (roll > 0) {
+    if (p1Turn) {
+      p1Pos = movePlayer(p1Pos, roll);
+      p1Score += p1Pos;
+
+      if (p1Score >= 21) {
+        return Point(1, 0);
+      }
+    } else {
+      p2Pos = movePlayer(p2Pos, roll);
+      p2Score += p2Pos;
+
+      if (p2Score >= 21) {
+        return Point(0, 1);
+      }
+    }
+  }
+
+  // roll amounts, and the number of times they appear
+  // this optimization, instead of a recursive call for every roll result,
+  // is necessary for the program to finish running
+  List<Point> allRolls = [
+    Point(3, 1),
+    Point(4, 3),
+    Point(5, 6),
+    Point(6, 7),
+    Point(7, 6),
+    Point(8, 3),
+    Point(9, 1)
+  ];
+
+  //print("-----------trying all rolls ------------");
+  Point result = Point(0, 0);
+  // try all possible outcomes
+  for (Point roll in allRolls) {
+    Point rollResult =
+        diracGame(p1Pos, p2Pos, p1Score, p2Score, roll.x.toInt(), !p1Turn);
+    result += Point(rollResult.x.toInt() * roll.y.toInt(),
+        rollResult.y.toInt() * roll.y.toInt());
+  }
+
+  return result;
+}
+
+int part1(int p1Pos, int p2Pos) {
   int p1Score = 0;
   int p2Score = 0;
 
@@ -19,7 +83,7 @@ void main(List<String> arguments) {
     p1Pos = movePlayer(p1Pos, p1Move);
     p1Score += p1Pos;
 
-    print("player 1: $p1Pos, $p1Score");
+    //print("player 1: $p1Pos, $p1Score");
 
     if (p1Score >= 1000) break;
 
@@ -27,16 +91,15 @@ void main(List<String> arguments) {
     p2Pos = movePlayer(p2Pos, p2Move);
     p2Score += p2Pos;
 
-    print("player 2: $p2Pos, $p2Score");
+    // print("player 2: $p2Pos, $p2Score");
   }
 
-  // player 1 won
   if (p1Score >= 1000) {
     print("player 1 won");
-    print(p2Score * dice.rollCount);
+    return p2Score * dice.rollCount;
   } else {
     print("player 2 won");
-    print(p1Score * dice.rollCount);
+    return p1Score * dice.rollCount;
   }
 }
 
